af27315ca554fa318aaf6d2d47298a5e9ff8c571
Blaga Lucian-Florin <blaga.lucianflorin@gmail.com>
2021-05-05 12:49:05 +0300

    Added photos' model, controller and migration

---
+++ app/Http/Controllers/PhotoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Photo;
+
+class PhotoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $photos = Photo::all ();
+
+        return (json_encode ($photos));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $newPhoto = new Photo;
+
+        $newPath = ($request -> all ())['path'];
+        $newPhoto['path'] = $newPath;
+        $newPhoto -> save ();
+
+        return (true);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $photo = Photo::findOrFail($id);
+
+        return json_encode (['path' => $photo['path']]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $photo   = Photo::findOrFail($id);
+        $newPath = ($request -> all ())['path'];
+
+        $photo['path'] = $newPath;
+        $photo -> save ();
+
+        return (true);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $photo = Photo::findOrFail($id);
+
+        $photo -> delete ();
+
+        return (true);
+    }
+}
