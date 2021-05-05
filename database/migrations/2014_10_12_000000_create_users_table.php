fa946ada683a811c6ce826103d7a4ae79cfe6924
Blaga Lucian-Florin <blaga.lucianflorin@gmail.com>
2021-05-05 12:45:54 +0300

    Refactor user create migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -11,16 +11,19 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function up()
+    public function up ()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->string('email', 191)->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::create ('users', function (Blueprint $table)
+        {
+            $table -> id ();
+            $table -> string ('email', 191) -> unique ();
+            $table -> string ('password');
+            $table -> string ('first_name');
+            $table -> string ('last_name');
+            $table -> string ('username') -> unique ();
+            $table -> timestamp ('created_at') -> default (DB::raw ('CURRENT_TIMESTAMP'));
+            $table -> foreignId ('photo_id') -> nullable ();
+            $table -> string ('token', 255) -> unique ();
         });
     }
 
@@ -29,8 +32,8 @@ class CreateUsersTable extends Migration
      *
      * @return void
      */
-    public function down()
+    public function down ()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists ('users');
     }
 }
