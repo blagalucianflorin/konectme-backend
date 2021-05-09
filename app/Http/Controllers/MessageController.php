6dd0c4f48855fbb6e4016d06afe0434c360927b9
alexandrafilip <alexandrafilip20@yahoo.com>
2021-05-09 14:37:58 +0300

    Updates

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -6,18 +6,18 @@ use App\Models\Message;
 use App\Models\Chat;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MessageController extends Controller
 {
     /**
      * Display a listing of the resource.
      *
-     * @return
+     * @return string
      */
     public function index()
     {
         $messages = Message::all();
-
         return (json_encode ($messages));
     }
 
@@ -26,7 +26,7 @@ class MessageController extends Controller
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @return
+     * @return string 
      */
     public function store(Request $request)
     {
@@ -36,7 +36,10 @@ class MessageController extends Controller
         $usersList        = json_decode ($verificationChat['users']);
 
         if (!in_array ($reqContent['sender_id'], $usersList))
-            return (json_encode (false));
+            return json_encode([
+                "success" => false, 
+                "message" => "This user is not in chat."
+            ]);
 
         $newMessage['sender_id'] = $reqContent['sender_id'];
         $newMessage['chat_id']   = $reqContent['chat_id'];
@@ -44,21 +47,27 @@ class MessageController extends Controller
 
         $newMessage -> save();
 
-        return (json_encode(true));
+        return json_encode([
+            "success" => true,
+            "message" => "None"
+        ]);
     }
 
     /**
      * Display the specified resource.
      *
      * @param  \App\Models\Message  $message
-     * @return
+     * @return string
      */
     public function show($id)
     {
         $message = Message::find($id);
 
         if ($message == null)
-            return (json_encode (false));
+            return json_encode([
+                "success" => false, 
+                "message" => "This message does not exist."
+            ]);
 
         return json_encode([
             'sender_id' => $message['sender_id'],
@@ -74,38 +83,95 @@ class MessageController extends Controller
      *
      * @param  \Illuminate\Http\Request  $request
      * @param  \App\Models\Message  $message
-     * @return
+     * @return string
      */
     public function update(Request $request, $id)
     {
         $message = Message::find ($id);
 
         if ($message == null)
-            return (json_encode (false));
+            return json_encode([
+                "success" => false, 
+                "message" => "This message does not exist."
+            ]);
 
         $newContent         = ($request -> all())['content'];
+
+        $token = $request -> bearerToken();
+        if($token == null)
+                return json_encode([
+                    "succes" => false,
+                    "message" => "User is not authenticated"
+                ]);
+        $user = DB::table('users') -> where ('token', $token)-> first();
+        
+        if ($user == null)
+        {
+            return (json_encode([
+                "succes" => false,
+                "message" => "Token does not exist"
+            ]));
+        }
+
+        if($user->id != $message['sender_id'])
+            return json_encode([
+                "succes" => false,
+                "message" => "Unauthorized access"
+            ]);
+
         $message['content'] = $newContent;
 
         $message -> save();
 
-        return (json_encode (true));
+        return json_encode([
+            "success" => true,
+            "message" => "None"
+        ]);
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Message  $message
-     * @return
+     * @return string
      */
-    public function destroy($id)
+    public function destroy(Request $request, $id)
     {
         $message = Message::find($id);
 
         if ($message == null)
-            return (json_encode (false));
+            return json_encode([
+                "success" => false, 
+                "message" => "This message does not exist."
+            ]);
+
+        $token = $request -> bearerToken();
+        if($token == null)
+            return json_encode([
+                "succes" => false,
+                "message" => "User is not authenticated"
+            ]);
+        $user = DB::table('users') -> where ('token', $token)-> first();
+
+        if ($user == null)
+        {
+            return (json_encode([
+                "succes" => false,
+                "message" => "Token does not exist"
+            ]));
+        }
+
+        if($user->id != $message['sender_id'])
+            return json_encode([
+                "succes" => false,
+                "message" => "Unauthorized access"
+            ]);
 
         $message -> delete();
 
-        return (json_encode (true));
+        return json_encode([
+            "success" => true,
+            "message" => "None"
+        ]);
     }
 }
