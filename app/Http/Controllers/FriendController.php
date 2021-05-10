9695513c05e1858298ec7f50e8f67c88572fd0fd
Blaga Lucian-Florin <blaga.lucianflorin@gmail.com>
2021-05-10 13:37:08 +0300

    Added friends list

---
+++ app/Http/Controllers/FriendController.php
@@ -0,0 +1,197 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Friend;
+use Illuminate\Http\Request;
+
+use App\Models\User;
+
+class FriendController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return string
+     */
+    public function index(Request $request)
+    {
+        //
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param Request $request
+     * @return string
+     */
+    public function show (Request $request): string
+    {
+        $reqData = $request -> all ();
+        $token   = $request -> bearerToken ();
+
+        if ($token == null)
+            return (json_encode ([
+                "success" => false,
+                "message" => "Unauthorized access"
+            ]));
+
+        $friendOne      = User::find ($reqData['friend_one']);
+        $friendTwo      = User::find ($reqData['friend_two']);
+        $friendOneCheck = User::where ('token', '=', $token) -> first ();
+
+        if ($friendOne == null || $friendTwo == null)
+            return (json_encode ([
+                "success" => false,
+                "message" => "User does not exist"
+            ]));
+
+        if ($friendOneCheck == null)
+            return (json_encode ([
+                "success" => false,
+                "message" => "Invalid token"
+            ]));
+
+        if ($friendOneCheck -> id != $friendOne -> id)
+            return (json_encode ([
+                "success" => false,
+                "message" => "Unauthorized access"
+            ]));
+
+        $areFriends = Friend::where ('friend_one_id', '=', $friendOne -> id) ->
+            where ('friend_two_id', '=', $friendTwo -> id)-> first () != null;
+        $areFriends |= Friend::where ('friend_one_id', '=', $friendTwo -> id) ->
+            where ('friend_two_id', '=', $friendOne -> id)-> first () != null;
+
+        if ($areFriends)
+            return (json_encode ([
+                "success" => true,
+                "message" => "Users are friends"
+            ]));
+        else
+            return (json_encode ([
+                "success" => true,
+                "message" => "Users are not friends"
+            ]));
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string
+     */
+    public function store(Request $request)
+    {
+        $reqData = $request -> all ();
+        $token   = $request -> bearerToken ();
+
+        if ($token == null)
+            return (json_encode ([
+                "success" => false,
+                "message" => "Unauthorized access"
+            ]));
+
+        $friendOne      = User::find ($reqData['friend_one']);
+        $friendTwo      = User::find ($reqData['friend_two']);
+        $friendOneCheck = User::where ('token', '=', $token) -> first ();
+
+        if ($friendOne == null || $friendTwo == null)
+            return (json_encode ([
+                "success" => false,
+                "message" => "User does not exist"
+            ]));
+
+        if ($friendOneCheck == null)
+            return (json_encode ([
+                "success" => false,
+                "message" => "Invalid token"
+            ]));
+
+        if ($friendOneCheck -> id != $friendOne -> id)
+            return (json_encode ([
+                "success" => false,
+                "message" => "Unauthorized access"
+            ]));
+
+        $alreadyFriends = Friend::where ('friend_one_id', '=', $friendOne -> id) ->
+                            where ('friend_two_id', '=', $friendTwo -> id)-> first () != null;
+        $alreadyFriends |= Friend::where ('friend_one_id', '=', $friendTwo -> id) ->
+                            where ('friend_two_id', '=', $friendOne -> id)-> first () != null;
+
+        if ($alreadyFriends)
+            return (json_encode ([
+                "success" => false,
+                "message" => "Users are already friends"
+            ]));
+
+        $newFriend = new Friend;
+
+        $newFriend -> friend_one_id = $reqData['friend_one'];
+        $newFriend -> friend_two_id = $reqData['friend_two'];
+
+        $newFriend -> save ();
+
+        return (json_encode ([
+            "success" => true,
+            "message" => "Users are now friends"
+        ]));
+    }
+
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Friend  $friend
+     * @return string
+     */
+    public function destroy (Request $request)
+    {
+        $reqData = $request -> all ();
+        $token   = $request -> bearerToken ();
+
+        if ($token == null)
+            return (json_encode ([
+                "success" => false,
+                "message" => "Unauthorized access"
+            ]));
+
+        $friendOne      = User::find ($reqData['friend_one']);
+        $friendTwo      = User::find ($reqData['friend_two']);
+        $friendOneCheck = User::where ('token', '=', $token) -> first ();
+
+        if ($friendOne == null || $friendTwo == null)
+            return (json_encode ([
+                "success" => false,
+                "message" => "User does not exist"
+            ]));
+
+        if ($friendOneCheck == null)
+            return (json_encode ([
+                "success" => false,
+                "message" => "Invalid token"
+            ]));
+
+        $friend = Friend::where ('friend_one_id', '=', $friendOne -> id) ->
+                where ('friend_two_id', '=', $friendTwo -> id)-> first ();
+
+        if ($friend == null)
+            $friend = Friend::where ('friend_one_id', '=', $friendTwo -> id) ->
+                where ('friend_two_id', '=', $friendOne -> id)-> first ();
+
+        if ($friend == null)
+            return (json_encode ([
+                "success" => false,
+                "message" => "Users aren't friends"
+            ]));
+
+        $friend -> delete ();
+
+        return (json_encode ([
+            "success" => true,
+            "message" => "Users aren't friends anymore"
+        ]));
+    }
+}
---
+++ database/migrations/2021_05_10_093542_create_friends_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFriendsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('friends', function (Blueprint $table) {
+            $table -> foreignId ("friend_one_id");
+            $table -> foreignId ("friend_two_id");
+
+            $table -> foreign ('friend_one_id') -> references ('id') -> on ('users');
+            $table -> foreign ('friend_two_id') -> references ('id') -> on ('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('friends');
+    }
+}
