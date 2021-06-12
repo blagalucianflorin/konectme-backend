a40d515f6ffe9819f6590ca09acf7a52a191410d
alexandrafilip <alexandrafilip20@yahoo.com>
2021-06-12 17:46:45 +0300

    Refactoring Message

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -16,12 +16,12 @@ class MessageController extends Controller
      *
      * @return string
      */
-    public function index()
+    public function index ()
     {
-        $messages = Message::all();
+        $messages = Message::all ();
+
         return (json_encode ($messages));
     }
-
 
     /**
      * Store a newly created resource in storage.
@@ -29,7 +29,7 @@ class MessageController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return string
      */
-    public function store(Request $request)
+    public function store (Request $request)
     {
         $newMessage       = new Message;
         $reqContent       = $request -> all ();
@@ -37,183 +37,176 @@ class MessageController extends Controller
         $usersList        = json_decode ($verificationChat['users']);
 
         if($reqContent['type'] == null || $reqContent['content'] == null)
-            return json_encode([
-                "succes" => false,
+            return (json_encode([
+                "succes"  => false,
                 "message" => "No content"
-            ]);
+            ]));
 
         if($reqContent['type'] == 'photo')
-                {
-                    $id = $reqContent['content'];
-                    $photo = Photo::find($id);
-                    if($photo == null)
-                        return json_encode([
-                            "succes" => false,
-                            "message" => "Invalid photo"
-                        ]);
-                }
+        {
+            $id    = $reqContent['content'];
+            $photo = Photo::find ($id);
+            if($photo == null)
+                return (json_encode([
+                    "succes"  => false,
+                    "message" => "Invalid photo"
+                ]));
+        }
 
         if (!in_array ($reqContent['sender_id'], $usersList))
-            return json_encode([
+            return (json_encode([
                 "success" => false,
                 "message" => "This user is not in chat."
-            ]);
+            ]));
 
         $newMessage['sender_id'] = $reqContent['sender_id'];
         $newMessage['chat_id']   = $reqContent['chat_id'];
         $newMessage['content']   = $reqContent['content'];
-        $newMessage['type']   = $reqContent['type'];
+        $newMessage['type']      = $reqContent['type'];
+        $newMessage -> save ();
 
-        // aici am expiry_time
+        $message = Message::find ($newMessage -> id);
+
         if ($reqContent['expiry_time'] != '0')
         {
-            $newTime = Date("Y-m-d H:i:s", strtotime(($reqContent['expiry_time']+180)." minutes", strtotime(Date("Y-m-d H:i:s"))));
-            //$newTime = $newMessage['sent_at']->addMinutes($reqContent['expiry_time']);
-            $newMessage['expiry_time'] = $newTime;
+            $newTime = Date ("Y-m-d H:i:s", strtotime (($reqContent['expiry_time'])." minutes", 
+                strtotime ($message['sent_at'])));
+            $message['expiry_time'] = $newTime;
         }    
         else 
         {
-            $newTime = Date("Y-m-d H:i:s", strtotime("999999999 minutes", strtotime( $newMessage['sent_at'])));
-            //$newTime = $newMessage['sent_at']->addMinutes($reqContent['expiry_time']);
-            $newMessage['expiry_time'] = $newTime;
+            $newTime = Date ("Y-m-d H:i:s", strtotime ("999999999 minutes", strtotime ($message['sent_at'])));
+            $message['expiry_time'] = $newTime;
         }    
      
-        $newMessage -> save();
+        $message -> save ();
 
-        return json_encode([
+        return (json_encode([
             "success" => true,
             "message" => "Message was sent"
-        ]);
+        ]));
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Message  $message
+     * @param  int  $id
      * @return string
      */
-    public function show($id)
+    public function show ($id)
     {
-        $message = Message::find($id);
-
+        $message = Message::find ($id);
         if ($message == null)
-            return json_encode([
+            return (json_encode([
                 "success" => false,
                 "message" => "This message does not exist."
-            ]);
+            ]));
 
-        return json_encode([
+        return (json_encode([
             'sender_id' => $message['sender_id'],
-            'chat_id' => $message['chat_id'],
-            'sent_at' => $message['sent_at'],
-            'content' => $message['content'],
-            'type' => $message['type']
-        ]);
+            'chat_id'   => $message['chat_id'],
+            'sent_at'   => $message['sent_at'],
+            'content'   => $message['content'],
+            'type'      => $message['type']
+        ]));
     }
-
 
     /**
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Message  $message
+     * @param  int  $id
      * @return string
      */
-    public function update(Request $request, $id)
+    public function update (Request $request, $id)
     {
         $message = Message::find ($id);
-
         if ($message == null)
-            return json_encode([
+            return (json_encode([
                 "success" => false,
                 "message" => "This message does not exist."
-            ]);
+            ]));
 
-        $newContent         = ($request -> all())['content'];
-        $newType            = ($request -> all())['type'];
-        
-
+        $newContent = ($request -> all())['content'];
+        $newType    = ($request -> all())['type'];
+      
         if($newType != $message['type'])
-            return json_encode([
+            return (json_encode([
                 "success" => false,
                 "message" => "Type of message can't be modified"
-            ]);
+            ]));
 
-        $token = $request -> bearerToken();
+        $token = $request -> bearerToken ();
         if($token == null)
-                return json_encode([
-                    "succes" => false,
-                    "message" => "User is not authenticated"
-                ]);
-        $user = DB::table('users') -> where ('token', $token)-> first();
-
-        if ($user == null)
-        {
             return (json_encode([
                 "succes" => false,
+                "message" => "User is not authenticated"
+            ]));
+
+        $user = DB::table('users') -> where ('token', $token) -> first ();
+        if ($user == null)
+            return (json_encode([
+                "succes"  => false,
                 "message" => "Token does not exist"
             ]));
-        }
-
-        if($user->id != $message['sender_id'])
-            return json_encode([
-                "succes" => false,
+        
+        if($user -> id != $message['sender_id'])
+            return (json_encode([
+                "succes"  => false,
                 "message" => "Unauthorized access"
-            ]);
+            ]));
 
         $message['content'] = $newContent['content'];
+        $message -> save ();
 
-        $message -> save();
-
-        return json_encode([
+        return (json_encode([
             "success" => true,
             "message" => "Message was edited"
-        ]);
+        ]));
     }
 
     /**
      * Remove the specified resource from storage.
      *
      * @param  \App\Models\Message  $message
+     * @param  int  $id
      * @return string
      */
-    public function destroy(Request $request, $id)
+    public function destroy (Request $request, $id)
     {
-        $message = Message::find($id);
-
+        $message = Message::find ($id);
         if ($message == null)
-            return json_encode([
+            return (json_encode([
                 "success" => false,
                 "message" => "This message does not exist."
-            ]);
+            ]));
 
-        $token = $request -> bearerToken();
+        $token = $request -> bearerToken ();
         if($token == null)
-            return json_encode([
-                "succes" => false,
-                "message" => "User is not authenticated"
-            ]);
-        $user = DB::table('users') -> where ('token', $token)-> first();
-
-        if ($user == null)
-        {
             return (json_encode([
-                "succes" => false,
+                "succes"  => false,
+                "message" => "User is not authenticated"
+            ]));
+
+        $user = DB::table('users') -> where ('token', $token)-> first ();
+        if ($user == null)
+            return (json_encode([
+                "succes"  => false,
                 "message" => "Token does not exist"
             ]));
-        }
+        
 
-        if($user->id != $message['sender_id'])
-            return json_encode([
-                "succes" => false,
+        if($user -> id != $message['sender_id'])
+            return (json_encode([
+                "succes"  => false,
                 "message" => "Unauthorized access"
-            ]);
+            ]));
 
-        $message -> delete();
+        $message -> delete ();
 
-        return json_encode([
+        return (json_encode([
             "success" => true,
             "message" => "Message deleted"
-        ]);
+        ]));
     }
 }
